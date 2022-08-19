439e5d02fc7b80fe3837d3badcde492a56d90fdd
adisonmarques <108470885+adisonmarques@users.noreply.github.com>
2022-08-19 12:09:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function calcSum() {
+    let num1 = document.getElementsByName("num1")[0].value;
+    let num2 = document.getElementsByName("num2")[0].value;
+    let num3 = document.getElementsByName("num3")[0].value;
+    let num4 = document.getElementsByName("num4")[0].value;
+    let sum = ((Number(num1) + Number(num2) + Number(num3))/3)*0.4 + (Number(num4))*0.6;
+    document.getElementsByName("sum")[0].value = sum;
+    document.getElementsByName("num5")[0].value = "Aprovado"  
+    if(sum>=6){document.getElementsByName("num5")[0].value = "Aprovado";} else{document.getElementsByName("num5")[0].value = "Reprovado";}             
+}
